a0bfbeae893a076159990e86522da8dfc2ebace0
swimmingwebdev <swimmingwebdev@gmail.com>
2025-02-02 12:47:12 -0800

    Update authentication service

--- README.md
+++ README.md
@@ -2,6 +2,50 @@
 
 #### Group3 
 - Team members
-  - Austin, Park
-  - Keziah, Wacnang
-  - Yang, Jung
+  - Austin Park
+  - Keziah Wacnang
+  - Yang Jung
+
+### Data Collection and Analytics System (Daily Expense Tracker)
+##  System Overview
+#### 1. Enter Data Service
+- Languages
+  - Frontend: ReactJS with tailwind
+  - Backend: Flask
+  - Database: MySQL
+- Features
+  - Send credentials to the Authentication service
+  - Users input
+    1. Date
+    2. Amount
+    3. Category(food, transport, rent, etc)
+    4. Payment Method(cash, credit card, debit card)
+  - Expenses are stored in MySQL
+
+#### 2. Show Results Service
+- Languages
+  - Frontend: ReactJS with tailwind
+  - Backend: NodeJS 
+- Features
+  - Send credentials to Authentication Service
+  - Retrieves analytics from MongoDB
+
+#### 3. Authentication Service
+- Languages
+  - Backend: Flask
+- Features
+  - Validate user credentials
+  - issue JWT tokens
+
+#### 4. Analytics Service
+- Languages
+  - Backend: Flask
+  - Database: MongoDB
+- Features
+  - Read data from MySQL
+  - Get analytics
+    1. Total spending
+    2. Average spending per category
+    3. Highest spending category
+    4. Monthly Reports
+  - Stores the results in MongoDB
---
+++ authentication/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, jsonify
+from datetime import datetime, timedelta, timezone
+import jwt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+USER_ID = os.getenv('USER_ID')
+USER_PASSWORD = os.getenv('USER_PASSWORD')
+
+#  when user tries to log in
+@app.route('/auth/login', methods=['POST'])
+def login():
+
+    username = request.json.get('username')
+    password = request.json.get('password')
+    
+    if not username or not password:
+        return jsonify({
+            'message': 'Please provide username and password'
+        }), 401
+    
+    if username == USER_ID and password == USER_PASSWORD:
+        # Create token that expires in 24 hours
+        token = jwt.encode({
+            'user': username,
+            'exp': datetime.now(timezone.utc) + timedelta(hours=24)
+        }, SECRET_KEY)
+        
+        return jsonify({
+            'token': token,
+            'message': 'Login successful'
+        })
+    
+    return jsonify({
+        'message': 'Wrong username or password'
+    }), 401
+
+# check if token is valid
+@app.route('/auth/verify', methods=['GET'])
+def verify():
+
+    # Get token from request header
+    # To prevent access protected routes without logging in
+    token = request.headers.get('Authorization')
+    
+    if not token:
+        return jsonify({
+            'message': 'Please provide token'
+        }), 401
+    
+    try:
+        # Check if the token is real and hasn't expired
+        jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return jsonify({
+            'message': 'Token is valid'
+        })
+    
+    except:
+        return jsonify({
+            'message': 'Token is not valid'
+        }), 401
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
